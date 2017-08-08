90bee8a2c870a444370106f5feddc28a9bc8f2d5
Philipp Klose <me@thehippo.de>
2017-08-08 16:29:37 +0200

    added verbose logging

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"path/filepath"
+	"time"
 
 	flags "github.com/jessevdk/go-flags"
 )
@@ -11,10 +13,21 @@ import (
 type cliOptions struct {
 	Port    uint   `short:"p" long:"port" description:"port to bind to" default:"8080"`
 	Address string `short:"b" long:"address" description:"address to bind to" default:"localhost"`
+	Verbose bool   `short:"v" long:"verbose" description:"log every request"`
 	// Directory string `short:"d" long:"directory" description:"folder to serve" default:"."`
 	Args struct {
 		Directory string `positional-arg-name:"directory" description:"directory to serve"`
 	} `positional-args:"yes"`
+}
+
+type statusWriter struct {
+	http.ResponseWriter
+	status int
+}
+
+func (st *statusWriter) WriteHeader(status int) {
+	st.status = status
+	st.ResponseWriter.WriteHeader(status)
 }
 
 func main() {
@@ -31,6 +44,21 @@ func main() {
 	}
 
 	fs := http.FileServer(http.Dir(options.Args.Directory))
-	fmt.Printf("Starting http-server at http://%s:%d\n", options.Address, options.Port)
+	if options.Verbose {
+		fs = logHandler(fs.ServeHTTP)
+	}
+	abs, _ := filepath.Abs(options.Args.Directory)
+	fmt.Printf("Serving %s at http://%s:%d\n", abs, options.Address, options.Port)
 	http.ListenAndServe(fmt.Sprintf("%s:%d", options.Address, options.Port), fs)
+}
+
+func logHandler(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		start := time.Now()
+		sw := &statusWriter{
+			ResponseWriter: w,
+		}
+		fn(sw, req)
+		fmt.Printf("Served \"%s\" in %v [%d]\n", req.RequestURI, time.Now().Sub(start), sw.status)
+	}
 }
