0b4c173e7811345ab91061f997b0a35f695529f5
Philipp Klose <me@thehippo.de>
2017-08-10 15:56:08 +0200

    added options to control cache settings

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ type cliOptions struct {
 	Port    uint   `short:"p" long:"port" description:"port to bind to" default:"8080"`
 	Address string `short:"b" long:"address" description:"address to bind to" default:"localhost"`
 	Verbose bool   `short:"v" long:"verbose" description:"log every request"`
+	Cache   int    `short:"c" long:"cache" description:"set cache headers (-1 for no cache)" default:"0"`
 	Args    struct {
 		Directory string `positional-arg-name:"directory" description:"directory to serve"`
 	} `positional-args:"yes"`
@@ -46,9 +47,33 @@ func main() {
 	if options.Verbose {
 		fs = logHandler(fs.ServeHTTP)
 	}
+	if options.Cache == -1 {
+		fs = noCacheHandler(fs.ServeHTTP)
+	}
+
+	if options.Cache > 0 {
+		fs = cacheHandler(options.Cache, fs.ServeHTTP)
+	}
+
 	abs, _ := filepath.Abs(options.Args.Directory)
 	fmt.Printf("Serving %s at http://%s:%d\n", abs, options.Address, options.Port)
 	http.ListenAndServe(fmt.Sprintf("%s:%d", options.Address, options.Port), fs)
+}
+
+func cacheHandler(cacheDuration int, fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Cache-Control", fmt.Sprintf("max-age=%d", cacheDuration))
+		fn(w, req)
+	}
+}
+
+func noCacheHandler(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Cache-Control", "no cache")
+		req.Header.Del("If-Modified-Since")
+		req.Header.Del("Cache-Control")
+		fn(w, req)
+	}
 }
 
 func logHandler(fn http.HandlerFunc) http.HandlerFunc {
